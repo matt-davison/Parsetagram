c19d8d88f6df2971b025b0448c57dbccdcd5ce3c
Matt Davison <mdavison@fb.com>
2020-07-10 13:38:36 -0400

    Add endless scrolling to home feed

--- app/src/main/java/com/mdavison/parsetagram/Fragments/PostsFragment.java
+++ app/src/main/java/com/mdavison/parsetagram/Fragments/PostsFragment.java
@@ -15,11 +15,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.mdavison.parsetagram.Activities.PostDetailsActivity;
 import com.mdavison.parsetagram.Adapters.PostsAdapter;
 import com.mdavison.parsetagram.Models.Post;
 import com.mdavison.parsetagram.R;
+import com.mdavison.parsetagram.Support.EndlessRecyclerViewScrollListener;
 import com.mdavison.parsetagram.Support.ItemClickSupport;
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -37,7 +39,7 @@ public class PostsFragment extends Fragment {
     private RecyclerView rvPosts;
     private List<Post> allPosts;
     protected SwipeRefreshLayout swipeContainer;
-    private final FragmentManager fragmentManager = getFragmentManager();
+    private EndlessRecyclerViewScrollListener scrollListener;
 
     public PostsFragment() {
         // Required empty public constructor
@@ -58,7 +60,10 @@ public class PostsFragment extends Fragment {
         allPosts = new ArrayList<>();
         postsAdapter = new PostsAdapter(getContext(), allPosts);
         rvPosts.setAdapter(postsAdapter);
-        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        rvPosts.setLayoutManager(linearLayoutManager);
+        queryPosts();
+
         swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -71,7 +76,7 @@ public class PostsFragment extends Fragment {
                 android.R.color.holo_green_light,
                 android.R.color.holo_orange_light,
                 android.R.color.holo_red_light);
-        queryPosts();
+
         ItemClickSupport.addTo(rvPosts).setOnItemClickListener(
                 new ItemClickSupport.OnItemClickListener() {
                     @Override
@@ -82,11 +87,38 @@ public class PostsFragment extends Fragment {
                     }
                 }
         );
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount,
+                                   RecyclerView view) {
+                queryMorePosts(allPosts.size());
+            }
+        };
+        rvPosts.addOnScrollListener(scrollListener);
     }
 
     protected void queryPosts() {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         query.include(Post.KEY_USER);
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATED);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> newPosts, ParseException e) {
+                swipeContainer.setRefreshing(false);
+                if (e != null) {
+                    Log.e(TAG, "Issues with getting posts", e);
+                    return;
+                }
+                postsAdapter.addAll(newPosts);
+            }
+        });
+    }
+
+    protected void queryMorePosts(int offset) {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.setSkip(offset);
         query.setLimit(20);
         query.addDescendingOrder(Post.KEY_CREATED);
         query.findInBackground(new FindCallback<Post>() {
